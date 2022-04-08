f888a0e77d83b0395c1f0970b2b0f738b14f35e7
RozuRoseMary <rozurosemary@gmail.com>
2022-04-08 15:58:41 +0700

    lab6.4 cp

--- src/App.js
+++ src/App.js
@@ -3,32 +3,15 @@ import "./App.css";
 function App(props) {
   return (
     <div className="App">
-      {/* box1 */}
-      <input
-        type="checkbox"
-        name="phoneBrand"
-        value="apple"
-        onClick={(event) => console.log(event.target.value)}
-      />
-      <label htmlFor="apple">Apple</label>
-      <br />
-      {/* box2 */}
-      <input
-        type="checkbox"
-        name="phoneBrand"
-        value="huawei"
-        onClick={(event) => console.log(event.target.value)}
-      />
-      <label htmlFor="huawei">Huawei</label>
-      <br />
-      {/* box3 */}
-      <input
-        type="checkbox"
-        name="phoneBrand"
-        value="samsung"
-        onClick={(event) => console.log(event.target.value)}
-      />
-      <label htmlFor="samsung">Samsung</label>
+      <a
+        href="https://google.com"
+        onClick={(event) => {
+          if (window.confirm("Go to google?")) return true;
+          else return event.preventDefault();
+        }}
+      >
+        Google
+      </a>
     </div>
   );
 }

5132e0d3adc8cefba696256ee152d0f0d733d734
RozuRoseMary <rozurosemary@gmail.com>
2022-04-08 16:24:38 +0700

    add solution lab6.3

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
 import "./App.css";
 
+// onClick={(event) => console.log(event.target.check)}
+
 function App(props) {
   return (
     <div className="App">
       {/* box1 */}
       <input
         type="checkbox"
-        name="phoneBrand"
+        name="apple"
         value="apple"
         onClick={(event) => console.log(event.target.value)}
       />
       <label htmlFor="apple">Apple</label>
       <br />
+
       {/* box2 */}
       <input
         type="checkbox"
-        name="phoneBrand"
+        name="huawei"
         value="huawei"
         onClick={(event) => console.log(event.target.value)}
       />
       <label htmlFor="huawei">Huawei</label>
       <br />
+
       {/* box3 */}
       <input
         type="checkbox"
-        name="phoneBrand"
+        name="samsung"
         value="samsung"
         onClick={(event) => console.log(event.target.value)}
       />

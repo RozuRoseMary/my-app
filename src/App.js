4132b766b32baf9b7aba4b4faff74b62cc6f3aa7
RozuRoseMary <rozurosemary@gmail.com>
2022-04-08 15:29:14 +0700

    lab6.2 cp

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import "./App.css";
 function App(props) {
   return (
     <div className="App">
-      <input
-        name="username"
-        placeholder="username"
+      <select
+        name="countries"
+        id="countries"
         onChange={(event) => console.log(event.target.value)}
-      />
+      >
+        <option value="thailand">Thailand</option>
+        <option value="japan">japan</option>
+        <option value="korea">Korea</option>
+        <option value="canada">Canada</option>
+        <option value="austria">Austria</option>
+      </select>
     </div>
   );
 }
